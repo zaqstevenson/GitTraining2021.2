6fc9f96c0bef49b6bd0efc1c75a95993677657c2
ZK <zacharkotler91@gmail.com>
2021-09-13 06:39:51 +0200

    input fields

--- salaryCalculator/force-app/main/default/lwc/salaryCalc/salaryCalc.js
+++ salaryCalculator/force-app/main/default/lwc/salaryCalc/salaryCalc.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class SalaryCalc extends LightningElement {}
+export default class SalaryCalc extends LightningElement {
+    invoiceBeforeTax;
+    netTakeHome;
+
+    handleInputInvoice(event) {
+        this.invoiceBeforeTax = event.target.value;
+        this.netTakeHome = this.invoiceBeforeTax * 100;
+    }
+
+}
