0f226aa58ce765f4107d3e5c61dee2dd7d05c0c0
ZK <zacharkotler91@gmail.com>
2021-09-14 19:48:28 +0200

    skladki b2b

--- salaryCalculator/force-app/main/default/lwc/salaryCalc/salaryCalc.js
+++ salaryCalculator/force-app/main/default/lwc/salaryCalc/salaryCalc.js
@@ -10,11 +10,29 @@ export default class SalaryCalc extends LightningElement {
     workingWhereLiving;
     podstawaOpodatkowania;
     kup = 250;
-    netTakeHome;
-
+    progPodatkowy = 85528;
+    zaliczkaNaPodatek;
+    netTakeHome2020;
+    sredniePrzecietneZarobki = 5259;
+    przecietneMiesieczneWynagrodzenie = 5656.51;
+    skladkaZdrowotna2021;
+    skladkaWypadkowa;
+    skladkaFP;
+    //Math.round((num + Number.EPSILON) * 100) / 100
     handleInputInvoice(event) {
         this.invoiceBeforeTax = event.target.value;
-        this.netTakeHome = this.invoiceBeforeTax * 100;
+        this.skladkaZdrowotna2021 = 
+            Math.round((this.przecietneMiesieczneWynagrodzenie * 0.75 * 0.09 + Number.EPSILON) * 100) / 100;
+        this.skladkaEmerytalna = 
+            Math.round((this.sredniePrzecietneZarobki * 0.6 * 0.1952 + Number.EPSILON) * 100) / 100;
+        this.skladkaRentowa = Math.round((this.sredniePrzecietneZarobki * 0.6 * 0.08 + Number.EPSILON) * 100) / 100;
+        this.skladkaWypadkowa = Math.round((this.sredniePrzecietneZarobki * 0.6 * 0.0167 + Number.EPSILON) * 100) / 100;
+        this.skladkaFP = Math.round((this.sredniePrzecietneZarobki * 0.6 * 0.0245 + Number.EPSILON) * 100) / 100;
+        console.log(this.skladkaZdrowotna2021);
+        console.log(this.skladkaEmerytalna);
+        console.log(this.skladkaRentowa);
+        console.log(this.skladkaWypadkowa);
+        console.log(this.skladkaFP);
     }
 
     handleInputSalary(event) {
@@ -28,15 +46,16 @@ export default class SalaryCalc extends LightningElement {
         this.podstawaOpodatkowania = this.salaryBeforeTax 
         - this.skladkaEmerytalna - this.skladkaRentowa - this.skladkaChorobowa - this.kup;
         this.podstawaOpodatkowania = Math.ceil(this.podstawaOpodatkowania);
-        console.log(this.podstawaOpodatkowania);
+        //console.log(this.podstawaOpodatkowania);
+        this.zaliczkaNaPodatek = this.podstawaOpodatkowania * 12
     }
 
-    handleWorkingWhereLiving(event) {
+    handleWorkingWhereLiving(event) { //TODO fix this
         this.workingWhereLiving = event.target.value;
         if (this.workingWhereLiving) {
-            kup = 250;
+            this.kup = 250;
         } else {
-            kup = 300;
+            this.kup = 300;
         }
     }
 
